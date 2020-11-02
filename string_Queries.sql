eaf51396297d961ec548971ce9ab0c959537e1da
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-11-02 14:36:20 -0500

    string data types and function practice sql

--- string_Queries.sql
+++ string_Queries.sql
@@ -63,3 +63,56 @@ set @middlename = 'Walker'
 set @lastname = 'Smith'
 
 select @firstname +' '+ @middlename +' '+ @lastname as FullName
+
+-- Another thing to do if middlename is null
+
+declare @firstname as nvarchar(20)
+declare @middlename as nvarchar(20)
+declare @lastname as nvarchar(20)
+
+set @firstname = 'Sarah'
+--set @middlename = 'Walker'
+set @lastname = 'Milligan'
+
+--select @firstname +' '+ @middlename +' '+ @lastname as FullName
+
+select @firstname + iif(@middlename is null, '',' ' + @middlename) +' '+ @lastname as FullName
+
+
+-- OR YOU CAN USE CONCAT
+SELECT CONCAT(@firstname,' '+ @middlename, ' ', @lastname) as FullName
+
+-- Join number to string 
+
+SELECT 'My Number is ' + convert(varchar(20), 4567)
+-- or
+SELECT 'My Number is ' + cast(4567 as varchar(20))
+
+-- CONVERT it to Currency
+
+SELECT 'my salary is: $' + convert(varchar(20), 2345.60) -- works but not well
+SELECT 'my salary is: ' + format(2345.60,'C') -- works best
+
+-- PRACTICE 
+
+--Add the letter A to the end of each name.
+
+--Add the letter Ⱥ to the end of each name. (You may want to copy and paste this letter, as it is an A with a stroke through it). If you are getting question marks, then give it another try.
+
+--Remove the first character from name.
+
+--Remove the last original character from name.
+
+
+
+--select [name] + 'A'
+--from sys.all_columns
+
+--select [name] + N'Ⱥ'
+--from sys.all_columns
+
+--select substring([name],2,len([name])-1) as [name]
+--from sys.all_columns
+
+--select substring([name],1,len([name])-1) as [name]
+--from sys.all_columns
