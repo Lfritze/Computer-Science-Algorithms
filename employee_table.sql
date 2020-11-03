4fed9955407e569b8a24efc497dea1975f954c09
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-11-03 11:35:46 -0500

    added additional sql queries for employee table

--- employee_table.sql
+++ employee_table.sql
@@ -59,3 +59,39 @@ SELECT * FROM tblEmployee
 --VALUES ('Jossef', 'H', 'Wright', 'TX593671R', '19711224', 'Litigation',131)
 
 SELECT * FROM tblEmployee
+
+-----
+-- INSERT 2 Employees ata atime into the table 
+INSERT INTO tblEmployee
+VALUES (1, 'Dylan', 'A', 'Word', 'HN513777D', '19920914', 'Customer Relations'),
+(2,'Jossef', 'H', 'Wright', 'TX593671R', '19711224', 'Litigation')
+
+-----
+
+SELECT * FROM tblEmployee WHERE [EmployeeLastName] = 'jones'
+
+SELECT * FROM tblEmployee WHERE [EmployeeLastName] >= 'word'
+
+-- NOTE: the % means and word that starts with 'w' and has as many letter after
+SELECT * FROM tblEmployee WHERE [EmployeeLastName] like 'w%'
+-- Note - this means 'w' is the second letter in the name
+SELECT * FROM tblEmployee WHERE [EmployeeLastName] like '_w%'
+-- NOte this puts w at the end of the name
+SELECT * FROM tblEmployee WHERE [EmployeeLastName] like '%w'
+-- NOte this check if w is anywhere in the name
+SELECT * FROM tblEmployee WHERE [EmployeeLastName] like '%w%'
+
+-- % = 0-infinity characters
+-- _ = 1 character
+-- [A-G] = In the range A-G.
+-- [AGQ] = A, G or Q.
+-- [^AGQ] = NOT A, G or Q.
+
+
+-- Note Nae starts with any letter between r through t
+Select * from tblEmployee where [EmployeeLastName] like '[r-t]%'
+-- NOTE ^ is the equivalent of NOT
+
+-- Note Employee NUmber <= 200
+select * from tblEmployee
+where EmployeeNumber <= 200
